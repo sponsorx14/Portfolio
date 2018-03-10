e73e6d1587bf948f842e0633cae1bcf58e583c15
Rafal <awrug3@gmail.com>
2018-03-10 17:31:36 +0100

    About section updated

--- js/index.js
+++ js/index.js
@@ -7,14 +7,14 @@ $(function(){
   },400);
 
   // CONSTANTS
-  const profileText = ['','','','J','u','n','i','o','r',' ','F','r','o','n','t',' ','E','n','d',' ','D','e','v','e','l','o','p','e','r',];
+  const profileText = ['','','','','','','J','u','n','i','o','r',' ','F','r','o','n','t',' ','E','n','d',' ','D','e','v','e','l','o','p','e','r',];
   const animateTextSpan = document.getElementById('animate-text');
 
   function printText(arr) {
     for (let i=0; i<arr.length; i++) {
         setTimeout(function() {
             animateTextSpan.append(...arr[i]);
-        }, 200 * i);
+        }, 150 * i);
     }
   }
 
