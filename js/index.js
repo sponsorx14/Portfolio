4fe34b03554cfb28445b13d2b3cc608168c79834
Rafal <awrug3@gmail.com>
2018-04-24 14:53:39 +0200

    Eslint added

--- js/index.js
+++ js/index.js
@@ -1,119 +1,110 @@
-$(function(){
-
+$(() => {
   // PROFILE TEXT CONSTANTS
   const profileText = 'Junior Front End Developer';
   const animateTextSpan = document.getElementById('animate-text');
 
-  //NAVBAR CONSTANTS
+  // NAVBAR CONSTANTS
   const nav = document.querySelector('.nav');
   const navTop = nav.offsetTop;
   const hamburger = document.querySelector('.hamburger');
 
-  //Contact CONSTANTS
+  // Contact CONSTANTS
   const input1 = document.querySelector('.input-1');
   const input2 = document.querySelector('.input-2');
   const textarea = document.querySelector('.textarea');
 
-  //Aside CONSTANTS
+  // Aside CONSTANTS
   const asideLinks = document.querySelector('.aside-links');
   const scrollTopButton = document.querySelector('.scroll-top');
 
   // FUNCTIONS
   function printText(text) {
-    for (let i=0; i<text.length; i++) {
-        setTimeout(function() {
-            animateTextSpan.append(...text[i]);
-        }, 100 * i);
+    for (let i = 0; i < text.length; i++) {
+      setTimeout(() => {
+        animateTextSpan.append(...text[i]);
+      }, 100 * i);
     }
   }
 
 
   function stickyNav() {
-    setTimeout(function(){
-      if(window.scrollY > navTop) {
+    setTimeout(() => {
+      if (window.scrollY > navTop) {
         document.body.classList.add('fixed-nav');
       } else {
         document.body.classList.remove('fixed-nav');
       }
-    },50)
+    }, 50);
   }
 
   function showAside() {
-    setTimeout(function(){
-      if(window.scrollY > 1300 && window.innerWidth > 1250){
+    setTimeout(() => {
+      if (window.scrollY > 1300 && window.innerWidth > 1250) {
         $(asideLinks).fadeIn();
       } else {
         $(asideLinks).fadeOut(200);
       }
-    },400)
+    }, 400);
   }
   function showScrollTopButton() {
-    setTimeout(function(){
-      if(window.scrollY > 800){
+    setTimeout(() => {
+      if (window.scrollY > 800) {
         $(scrollTopButton).fadeIn();
       } else {
         $(scrollTopButton).fadeOut(200);
       }
-    },400)
+    }, 400);
   }
 
   function inputAnimation() {
-    if(this.value != '' ){
+    if (this.value !== '') {
       this.classList.add('has-content');
-    }else {
+    } else {
       this.classList.remove('has-content');
     }
-  }
-
-  function getActive(e){
-    e.preventDefault();
-    $(this).removeClass('active');
   }
 
   function smoothScroll(e) {
     $('.hamburger').removeClass('is-active');
     $('.navbar').removeClass('navbar-collapse');
     e.preventDefault();
-    let target = this.hash;
-    $target = $(target);
+    const target = this.hash;
+    const $target = $(target);
     $('html, body').stop().animate({
-    'scrollTop': $target.offset().top
-    }, 900, 'swing', function () {
-    window.location.hash = target;
-  });
+      scrollTop: $target.offset().top,
+    }, 900, 'swing', () => {
+      window.location.hash = target;
+    });
   }
 
-  //Loader
-  window.addEventListener('load', function(){
-    $('.loader').fadeOut(400, function() {
+  // Loader
+  window.addEventListener('load', () => {
+    $('.loader').fadeOut(400, function () {
       $(this).remove();
       printText(profileText);
     });
   });
 
-  //stickyNav
-  window.addEventListener('scroll', function(){
+  // stickyNav
+  window.addEventListener('scroll', () => {
     stickyNav();
     showAside();
     showScrollTopButton();
-
   });
-  hamburger.addEventListener('click', function(){
+  hamburger.addEventListener('click', function () {
     this.classList.toggle('is-active');
     $('.navbar').toggleClass('navbar-collapse');
+  });
 
-  })
-
-  //Contact Input Animation
+  // Contact Input Animation
   input1.addEventListener('focusout', inputAnimation);
   input2.addEventListener('focusout', inputAnimation);
   textarea.addEventListener('focusout', inputAnimation);
 
 
-  //Smooth scroll events
+  // Smooth scroll events
   $('.scroll-top-button').on('click', smoothScroll);
   $('.jumbotron-arrow').on('click', smoothScroll);
   $('.nav-links').on('click', smoothScroll);
   $('.contact-button').on('click', smoothScroll);
-
 });
